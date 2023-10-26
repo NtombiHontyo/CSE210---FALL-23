20f19788f9e2ca60ceff5fe3cfca472032a5077e
NtombiHontyo <u252121986@byui.edu>
2023-10-26 04:48:53 +0200

    lesson 2 programme assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answerFromUser = Console.ReadLine();
+        int gradePercentage = int.Parse(answerFromUser);
+        
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (gradePercentage >= 80 )
+        {
+            letter = "B"; 
+        }
+        else if (gradePercentage >= 70 )
+        {
+            letter = "C"; 
+        }
+        else if (gradePercentage >= 60 )
+        {
+            letter = "D"; 
+        }
+        else if (gradePercentage < 60 )
+        {
+            letter = "F"; 
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class, please try harder next time.");
+        }
     }
 }
