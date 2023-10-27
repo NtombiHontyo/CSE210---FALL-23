cc62c21e8daf3ce7b8d8d420c45f640f762691da
NtombiHontyo <u252121986@byui.edu>
2023-10-27 23:39:59 +0200

    Lesson for loops

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int runningTotal = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        Console.Write("Enter number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        numbers.Add(userNumber);
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput); 
+            numbers.Add(userNumber);  
+        }
+        numbers.Remove(0);
+        foreach (int number in numbers)
+        {
+            runningTotal += number;
+        }  
+
+        Console.WriteLine($"The sum is: {runningTotal}"); 
+
+        double total = runningTotal;
+        double noOfNumbers = numbers.Count;
+        double average = total / noOfNumbers;
+        Console.WriteLine($"The average is: {average}"); 
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
