200a6db7bbf5252a86ae6fe37ae7ef751f1d04ac
NtombiHontyo <u252121986@byui.edu>
2023-10-28 20:11:03 +0200

    Lesson on Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string answer = Console.ReadLine();
+            int favNumber = int.Parse(answer);
+            return favNumber;
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int numberSquared = userNumber * userNumber;
+            return numberSquared;
+        }
+
+        
+
+
+        DisplayWelcome();
+        //PromptUserName();
+        //PromptUserNumber();
+        
+        static void DisplayResult()
+        {
+            
+            Console.WriteLine($"{PromptUserName()}, the square of your number is: {SquareNumber(PromptUserNumber())}");
+        }
+
+        DisplayResult();
+
+        
     }
 }
